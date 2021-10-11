0a4b6693d50255d13930500311c070299aec0e6f
Marina Monsivais <minamonsi@me.com>
2021-10-10 21:30:40 -0500

    WIP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -11,21 +11,61 @@ router.get('/',(req,res) =>{
     })
 })
 
-router.get('/',(req,res,next) =>{
-    next()
+router.get('/:id', async(req, res) => {
+    try {
+        const action = await Actions.get(req.actionToBody.id)
+        if(!action) {
+            res.status(404).json({
+                message:"Not found", 
+            })
+        }
+    } catch (err){
+        res.status(500).json({
+            message: "The project with the specified ID does not exist",
+            err: err.message,
+            stack: err.stack,
+    })
+  }
 })
 
-router.post('/',(req,res,next) =>{
-    next()
+
+router.post('/', (req, res) => {
+    const newProject = req.body
+    if (!newProject || !newProject.id) {
+        res.status(400).json({
+            message:'Please provide name or description.',
+        })
+    } else {
+        Actions.insert(newProject)
+        .then(createdProject =>{
+            res.json(createdProject)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Server error",
+                err: err.message,
+                Stack: err.stack,
+            })
+        })
+    }
 })
 
 router.put('/',(req,res,next) =>{
     next()
 })
 
-router.delete('/',(req,res,next) =>{
-    next()
+router.delete('/:id', async (req, res) => {
+    try{
+       throw new Error('sad') 
+    }catch (err) {
+        res.status(500).json({
+            message: "Server error",
+            err: err.message,
+            Stack: err.stack,
+        })
+    }
 })
+
 
 //error handling router
 router.use((err,req,res)=>{
